6b41e34e3386bb5a494ab91feda0e8bd5e1f0325
Gunnar K. Halvorsen <gunnar.halvorsen@bouvet.no>
2024-10-29 12:48:15 +0100

    mobie refinement

--- menu.js
+++ menu.js
@@ -1,7 +1,16 @@
 // menu.js
 function toggleMenu() {
   const rightSection = document.getElementById('right-section');
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  
   rightSection.classList.toggle('show');
+
+  // Toggle text between hamburger and cross
+  if (rightSection.classList.contains('show')) {
+    hamburgerMenu.innerHTML = '✖'; // Cross
+  } else {
+    hamburgerMenu.innerHTML = '☰'; // Hamburger
+  }
 }
 
 document.getElementById('hamburger-menu').onclick = toggleMenu;
